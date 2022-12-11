e30de44f7b04edbd577944c09b20346ef36b3c02
Kenya Chan <kenya.chan@gmail.com>
2022-12-11 19:38:21 +1100

    Add get DOM element method to list object

--- src/modules/list.js
+++ src/modules/list.js
@@ -1,3 +1,5 @@
+import { Task } from './task';
+
 export const List = (name) => {
 
 	const tasks = [];
@@ -12,6 +14,28 @@ export const List = (name) => {
 
 	const getTasks = () => {
 		return tasks;
+	}
+
+	const getDOMElement = () => {
+		const listElement = document.createElement('div');
+		listElement.classList.add('list');
+
+		const listName = document.createElement('p');
+		listName.classList.add('list-name');
+		listName.textContent = getName();
+		listElement.appendChild(listName);
+
+		const itemContainer = document.createElement('div');
+		itemContainer.classList.add('item-container');
+		listElement.appendChild(itemContainer);
+
+		// render list elements
+		
+		tasks.forEach(task => {
+			listElement.appendChild(task.getDOMElement());
+		});
+
+		return listElement;
 	}
 
 	const add = (item) => {
@@ -30,6 +54,7 @@ export const List = (name) => {
 		setName,
 		getName,
 		getTasks,
+		getDOMElement,
 		add,
 		remove,
 	}
