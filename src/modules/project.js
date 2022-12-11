e69bdc887f6f44e62c415f49a875d27a57859167
Kenya Chan <kenya.chan@gmail.com>
2022-12-12 09:10:35 +1100

    Add project object

---
+++ src/modules/project.js
@@ -0,0 +1,35 @@
+export const newProject = (name) => {
+	const tasks = [];
+
+	const getTasks = () => {
+		return tasks;
+	}
+
+	const getName = () => {
+		return name;
+	}
+
+	const setName = () => {
+		name = newName;
+	}
+
+	const add = (task) => {
+		tasks.push(task);
+	}
+
+	const remove = (task) => {
+		const index = task.indexOf(task);
+
+		if (index > -1) {
+			tasks.splice(index, 1);
+		}
+	}
+
+	return {
+		getTasks,
+		getName,
+		setName,
+		add,
+		remove,
+	}
+}
