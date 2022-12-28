bb69726518ca0e1f8f1baf39c9ca283bedc48044
Kenya Chan <kenya.chan@gmail.com>
2022-12-29 00:19:38 +1100

    Remove redundant code

--- src/modules/project.js
+++ src/modules/project.js
@@ -40,38 +40,4 @@ export const Project = (() => {
 	};
 })();
 
-export const newProject = (name) => {
-	const tasks = [];
 
-	const getTasks = () => {
-		return tasks;
-	}
-
-	const getName = () => {
-		return name;
-	}
-
-	const setName = () => {
-		name = newName;
-	}
-
-	const add = (task) => {
-		tasks.push(task);
-	}
-
-	const remove = (task) => {
-		const index = task.indexOf(task);
-
-		if (index > -1) {
-			tasks.splice(index, 1);
-		}
-	}
-
-	return {
-		getTasks,
-		getName,
-		setName,
-		add,
-		remove,
-	}
-}
