aaa7c3bebeff30e1a5f5fb5da9dff31c2fb1e300
Kenya Chan <kenya.chan@gmail.com>
2023-01-02 10:46:21 +1100

    separate rendering icons to it's own module

---
+++ src/modules/icons.js
@@ -0,0 +1,59 @@
+const svgNS = 'http://www.w3.org/2000/svg';
+
+export const list = () => {
+	const icon = document.createElementNS(svgNS, 'svg');
+	
+	icon.setAttribute('xmlns', svgNS);
+	icon.setAttribute('width', '48px');
+	icon.setAttribute('height', '48px');
+	icon.setAttribute('viewBox', '0 0 48 48');
+
+	const path = document.createElementNS(svgNS, 'path');
+	path.setAttribute('d', 'M7.5 34q-.65 0-1.075-.425Q6 33.15 6 32.5q0-.65.425-1.075Q6.85 31 7.5 31q.65 0 1.075.425Q9 31.85 9 32.5q0 .65-.425 1.075Q8.15 34 7.5 34Zm0-8.5q-.65 0-1.075-.425Q6 24.65 6 24q0-.65.425-1.075Q6.85 22.5 7.5 22.5q.65 0 1.075.425Q9 23.35 9 24q0 .65-.425 1.075-.425.425-1.075.425Zm0-8.5q-.65 0-1.075-.425Q6 16.15 6 15.5q0-.65.425-1.075Q6.85 14 7.5 14q.65 0 1.075.425Q9 14.85 9 15.5q0 .65-.425 1.075Q8.15 17 7.5 17Zm7 17v-3H42v3Zm0-8.5v-3H42v3Zm0-8.5v-3H42v3Z');
+	icon.appendChild(path);
+
+	return icon;
+}
+
+export const edit = () => {
+	const icon = document.createElementNS(svgNS, 'svg');
+
+	icon.setAttribute('xmlns', svgNS);
+	icon.setAttribute('width', '48px');
+	icon.setAttribute('height', '48px');
+	icon.setAttribute('viewBox', '0 0 48 48');
+
+	const path = document.createElementNS(svgNS, 'path');
+	path.setAttribute('d','M9 39h2.2l22.15-22.15-2.2-2.2L9 36.8Zm30.7-24.3-6.4-6.4 2.1-2.1q.85-.85 2.1-.85t2.1.85l2.2 2.2q.85.85.85 2.1t-.85 2.1Zm-2.1 2.1L12.4 42H6v-6.4l25.2-25.2Zm-5.35-1.05-1.1-1.1 2.2 2.2Z');
+	icon.appendChild(path);
+
+	return icon;
+}
+
+export const logo = () => {
+	const logoElement = document.createElement('div');
+	logoElement.classList.add('logo');
+
+	const icon = document.createElementNS(svgNS, 'svg');
+	icon.setAttribute('width', '16px');
+	icon.setAttribute('height', '16px');
+	icon.setAttribute('viewBox', '0 0 16 16');
+	icon.setAttribute('xmlns', svgNS);
+	icon.setAttribute('version', '1.1');
+	icon.setAttribute('fill', 'none');
+	icon.setAttribute('stroke', 'currentColor');
+	icon.setAttribute('stroke-linecap', 'round');
+	icon.setAttribute('stroke-linejoin', 'round');
+	icon.setAttribute('stroke-width', '1.5');
+	logoElement.appendChild(icon);
+	
+	const path = document.createElementNS(svgNS, 'path');
+	path.setAttribute('d','m1.75 9.75 2.5 2.5m3.5-4 2.5-2.5m-4.5 4 2.5 2.5 6-6.5');
+	icon.appendChild(path);
+	
+	const logoText = document.createElement('p');
+	logoText.textContent = 'ToDo';
+	logoElement.appendChild(logoText);
+
+	return logoElement;
+}
--- src/modules/render.js
+++ src/modules/render.js
@@ -1,44 +1,34 @@
-export const listIcon = () => {
-	const icon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-	
-	icon.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-	icon.setAttribute('width', '48px');
-	icon.setAttribute('height', '48px');
-	icon.setAttribute('viewBox', '0 0 48 48');
-
-	const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-	path.setAttribute('d', 'M7.5 34q-.65 0-1.075-.425Q6 33.15 6 32.5q0-.65.425-1.075Q6.85 31 7.5 31q.65 0 1.075.425Q9 31.85 9 32.5q0 .65-.425 1.075Q8.15 34 7.5 34Zm0-8.5q-.65 0-1.075-.425Q6 24.65 6 24q0-.65.425-1.075Q6.85 22.5 7.5 22.5q.65 0 1.075.425Q9 23.35 9 24q0 .65-.425 1.075-.425.425-1.075.425Zm0-8.5q-.65 0-1.075-.425Q6 16.15 6 15.5q0-.65.425-1.075Q6.85 14 7.5 14q.65 0 1.075.425Q9 14.85 9 15.5q0 .65-.425 1.075Q8.15 17 7.5 17Zm7 17v-3H42v3Zm0-8.5v-3H42v3Zm0-8.5v-3H42v3Z');
-	icon.appendChild(path);
-
-	return icon;
-}
+import * as icons from './icons'; 
 
 export const taskElement = (taskObj) => {
 	const element = document.createElement('li');
 	element.dataset.uuid = taskObj.uuid();
-	
+
+	const itemCompleteButton = document.createElement('input');
+	itemCompleteButton.setAttribute('type', 'checkbox');
+	element.appendChild(itemCompleteButton);
+
 	const itemName = document.createElement('p');
 	itemName.classList.add('name');
 	itemName.textContent = taskObj.getName();
 	element.appendChild(itemName);
 
-	const itemButtons = document.createElement('div');
-	itemButtons.classList.add('buttonsContainer');
-	element.appendChild(itemButtons);
-
 	const itemPriority = document.createElement('p');
 	itemPriority.classList.add('priority');
 	itemPriority.textContent = taskObj.getPriority();
-	itemButtons.appendChild(itemPriority);
+	element.appendChild(itemPriority);
 
 	const itemDueDate = document.createElement('p');
 	itemDueDate.classList.add('dueDate');
 	itemDueDate.textContent = taskObj.getDueDate();
-	itemButtons.appendChild(itemDueDate);
+	element.appendChild(itemDueDate);
 
-	const itemCompleteButton = document.createElement('button');
-	itemCompleteButton.textContent = '.';
-	itemButtons.appendChild(itemCompleteButton);
+	const editButton = document.createElement('button');
+	editButton.classList.add('editButton');
+	element.appendChild(editButton);
+
+	const editIcon = icons.edit();
+	editButton.appendChild(editIcon);
 
 	return element;
 }
@@ -67,7 +57,7 @@ export const projectListItemElement = (projectName) => {
 	element.appendChild(listItemInner);
 
 	const listItemIcon = document.createElement('span');
-	const icon = listIcon();
+	const icon = icons.list();
 	listItemIcon.appendChild(icon);
 	listItemInner.appendChild(listItemIcon);
 
@@ -92,38 +82,10 @@ export const projectListElement = () => {
 	return element;
 }
 
-export const logo = () => {
-	const logoElement = document.createElement('div');
-	logoElement.classList.add('logo');
-
-	const icon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-	icon.setAttribute('width', '16px');
-	icon.setAttribute('height', '16px');
-	icon.setAttribute('viewBox', '0 0 16 16');
-	icon.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-	icon.setAttribute('version', '1.1');
-	icon.setAttribute('fill', 'none');
-	icon.setAttribute('stroke', 'currentColor');
-	icon.setAttribute('stroke-linecap', 'round');
-	icon.setAttribute('stroke-linejoin', 'round');
-	icon.setAttribute('stroke-width', '1.5');
-	logoElement.appendChild(icon);
-	
-	const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-	path.setAttribute('d','m1.75 9.75 2.5 2.5m3.5-4 2.5-2.5m-4.5 4 2.5 2.5 6-6.5');
-	icon.appendChild(path);
-	
-	const logoText = document.createElement('p');
-	logoText.textContent = 'ToDo';
-	logoElement.appendChild(logoText);
-
-	return logoElement;
-}
-
 export const header = () => {
 	const headerElement = document.createElement('header');
 
-	const headerLogo = logo();
+	const headerLogo = icons.logo();
 	headerElement.appendChild(headerLogo);
 
 	return headerElement;
